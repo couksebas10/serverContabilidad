d927dd700a23d1eda67766fcebf29fdbd93cf4b7
Sebastian henao <sbitashenaol@gmail.com>
2019-02-21 11:36:36 -0500

    server

--- server.js
+++ server.js
@@ -1,25 +1,38 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 var app = express()
 
 var port = 3030
 var host = 'localhost'
 
-mongoose.connect('mongodb://127.0.0.1:27017/contabilidad');
-var db = mongoose.connection;
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 
-db.on('error', console.error.bind(console, 'connection error:'));
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+    next()
+})
+
+mongoose.connect('mongodb://127.0.0.1:27017/contabilidad')
+var db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'connection error:'))
 
 db.once('open', function () {
     app.get('/', (req, res) => {
-        db.collection('usuarios').find().toArray((err, result) => {
-            res.send(result)
-        })
+        res.send('Bienvenido Servidor Contabilidad !')
     })
 })
 
-app.get('/getUsuarios', () => {
-
+app.get('/getUsuarios', (req, res) => {
+    db.collection('usuarios').find().toArray((err, result) => {
+        res.send(result)
+    })
 })
 
 app.listen(port, host)
